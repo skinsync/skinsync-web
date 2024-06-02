6805866047cc019fabc57c5d75a59740f1cdbe41
naufaldzakiim <naufalimtiyaz35@gmail.com>
2024-06-02 18:54:35 +0700

    fix config for build in app engine

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,13 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: "_next",
+  generateBuildId: async () => {
+      if (process.env.BUILD_ID) {
+          return process.env.BUILD_ID;
+      } else {
+          return `${new Date().getTime()}`;
+      }
   },
 };
 
-export default config;
+export default nextConfig;
